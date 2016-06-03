b0925d316b290c3eb29553bece41d177809c6b1f
Ryan Giordano <Ryan Giordano>
2016-06-02 23:16:15 -0400

    Second push

--- app/users/profile.controller.js
+++ app/users/profile.controller.js
@@ -1,7 +1,8 @@
 angular.module('Lozone')
-.controller('ProfileCtrl', function($state, auth, profile, md5){
+.controller('ProfileCtrl', function($state, auth, profile, md5, Users){
   var profileCtrl = this;
   profileCtrl.profile = profile;
+  profileCtrl.getGravatar = Users.getGravatar;
   profileCtrl.updateProfile = function(){
     profileCtrl.profile.emailHash = md5.createHash(auth.password.email);
     profileCtrl.profile.$save();

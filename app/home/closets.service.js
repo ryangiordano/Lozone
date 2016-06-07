0d3bbf94fb74fd93e2e7d24e6a46adbfc9cdf4bd
Ryan Giordano <rgiorda1@gmail.com>
2016-06-06 23:58:32 -0400

    test

--- app/home/closets.service.js
+++ app/home/closets.service.js
@@ -2,6 +2,10 @@ angular.module('Lozone')
 .factory('Closets', function($firebaseArray, FirebaseUrl){
   var ref = new Firebase(FirebaseUrl + "closets");
   var closets = $firebaseArray(ref);
-
+  return{
+    forCloset: function(closetId){
+      return $firebaseArray(ref.child(closetId))
+    }
+  }
   return closets;
 });
